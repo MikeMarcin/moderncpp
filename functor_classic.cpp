b51c63e27e04c09328eb7f59b0517fef630e47f8
Michael Marcin <michael.marcin@escalation.com>
2018-07-19 11:52:51 -0500

    add more detail

--- functor_classic.cpp
+++ functor_classic.cpp
@@ -15,7 +15,27 @@ struct Shape {
 	ShapeType type;
 };
 
-// writing functors often involves a lot of uninteresting boilerplate
+ShapeType ShapeOfTheDay() { return  SHAPE_RHOMBUS; }
+
+struct Dog {
+	bool good = true;
+};
+
+struct Cat {
+	bool good = false;
+};
+
+//////////////////////////////////////////////////////////////////////////
+// Functor 1/2
+//////////////////////////////////////////////////////////////////////////
+// writing functors involves a lot of uninteresting boilerplate
+class IsCircle {
+public:
+	bool operator()(const Shape& shape) const {
+		return shape.type == SHAPE_CIRCLE;
+	}
+};
+
 class IsShapeType {
 public:
 	IsShapeType(ShapeType type_) : type(type_) {}
@@ -26,10 +46,37 @@ private:
 	ShapeType type;
 };
 
+//////////////////////////////////////////////////////////////////////////
+// Generic Functor 1/2
+//////////////////////////////////////////////////////////////////////////
+class IsGood {
+public:
+	template< typename T >
+	bool operator()( const T& x ) const {
+		return x.good;
+	}
+};
+
+
 int main() {
 	std::vector<Shape> shapes{SHAPE_CIRCLE, SHAPE_SQUARE, SHAPE_CIRCLE, SHAPE_TRIANGLE, SHAPE_RHOMBUS};
 
-	auto numCircles = std::count_if(begin(shapes), end(shapes), IsShapeType{SHAPE_CIRCLE});
+	//////////////////////////////////////////////////////////////////////////
+	// Functor 2/2
+	//////////////////////////////////////////////////////////////////////////
+	auto numCircles = std::count_if(begin(shapes), end(shapes), IsCircle{});
 	std::cout << "There are " << numCircles << " circles" << std::endl;
-	return 0;
+
+	// functors with variables
+	auto numShapeOfTheDay = std::count_if(begin(shapes), end(shapes), IsShapeType{ShapeOfTheDay()});
+	std::cout << "There are " << numShapeOfTheDay << " shapes of the day" << std::endl;
+
+	//////////////////////////////////////////////////////////////////////////
+	// Generic Functor 2/2
+	//////////////////////////////////////////////////////////////////////////
+	auto isGood = IsGood{};
+	Dog dog;
+	Cat cat;
+	std::cout << "Dog is good? " << isGood(dog) << std::endl;
+	std::cout << "Cat is good? " << isGood(cat) << std::endl;
 }
--- functor_modern.cpp
+++ functor_modern.cpp
@@ -15,15 +15,43 @@ struct Shape {
 	ShapeType type;
 };
 
+ShapeType ShapeOfTheDay() { return  SHAPE_RHOMBUS; }
+
+struct Dog {
+	bool good = true;
+};
+
+struct Cat {
+	bool good = false;
+};
+
+
 int main() {
 	std::vector<Shape> shapes{SHAPE_CIRCLE, SHAPE_SQUARE, SHAPE_CIRCLE, SHAPE_TRIANGLE, SHAPE_RHOMBUS};
 
+	//////////////////////////////////////////////////////////////////////////
+	// Lambda
+	//////////////////////////////////////////////////////////////////////////
 	// notice how concise the lambda form is relative to the explicit functor class
-	auto isCircle = [](auto& shape){ return shape.type == SHAPE_CIRCLE; };
-
+	auto isCircle = [](const Shape& shape) { return shape.type == SHAPE_CIRCLE; };
 	auto numCircles = std::count_if(begin(shapes), end(shapes), isCircle);
 	std::cout << "There are " << numCircles << " circles" << std::endl;
-	return 0;
+
+	//////////////////////////////////////////////////////////////////////////
+	// Lambda Captures
+	//////////////////////////////////////////////////////////////////////////
+	auto isShapeOfTheDay = [shapeOfTheDay = ShapeOfTheDay()](const Shape& shape) { return shape.type == shapeOfTheDay; };
+	auto numShapeOfTheDay = std::count_if(begin(shapes), end(shapes), isShapeOfTheDay);
+	std::cout << "There are " << numShapeOfTheDay << " shapes of the day" << std::endl;
+
+	//////////////////////////////////////////////////////////////////////////
+	// Generic Lambda
+	//////////////////////////////////////////////////////////////////////////
+	auto isGood = [](const auto& x){  return x.good; };
+	Dog dog;
+	Cat cat;
+	std::cout << "Dog is good? " << isGood(dog) << std::endl;
+	std::cout << "Cat is good? " << isGood(cat) << std::endl;
 }
 
 
